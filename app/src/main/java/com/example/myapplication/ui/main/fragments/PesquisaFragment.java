ccee69ab6bb46f26b02b6107a480b3597386e5ca
Tiago <tsilva727@gmail.com>
2021-05-04 23:04:42 -0300

    botão pesquisar compilando informações

--- app/src/main/java/com/example/myapplication/ui/main/fragments/PesquisaFragment.java
+++ app/src/main/java/com/example/myapplication/ui/main/fragments/PesquisaFragment.java
@@ -1,14 +1,26 @@
 package com.example.myapplication.ui.main.fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewDebug;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.myapplication.R;
+import com.example.myapplication.adapter.PageAdapterFragment;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import static android.app.ProgressDialog.show;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,9 +34,15 @@ public class PesquisaFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private ListView list;
+    private Adapter adapter;
+    private Button btnPesquisar,btnLimpar;
+    private Button radButton1,radButton2,radButton3,radButton4;
+    private Button radGro1,radGro2;
 
     public PesquisaFragment() {
         // Required empty public constructor
@@ -51,16 +69,31 @@ public class PesquisaFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+            mParam1 = this.mParam1;
+            mParam2 = this.mParam2;
+            btnPesquisar = this.btnPesquisar;
+
+
+    }
+    @SuppressLint("ResourceType")
+    public void btnPesquisarOnClick(View v){
+        Button button = (Button) btnPesquisar.findViewById(R.layout.activity_main);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+
+            }
+        });
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pesquisa, container, false);
+
     }
+
 }
